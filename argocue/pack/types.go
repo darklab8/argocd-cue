0b84e4389cb76559d59fd41c2a3b6de90a90ec17
dd84ai <dd84ai@gmail.com>
2024-03-17 21:10:57 +0100

    refactor: to more detailed error

--- argocue/pack/types.go
+++ argocue/pack/types.go
@@ -5,14 +5,16 @@ import (
 	"os"
 	"strings"
 
+	"github.com/darklab8/argocd-cue/argocue/logus"
+	"github.com/darklab8/go-typelog/typelog"
 	"github.com/darklab8/go-utils/goutils/utils/utils_types"
 )
 
-type Package int64
+type PackageType string
 
 const (
-	Helm Package = iota
-	Manifests
+	Helm      PackageType = "helm_tool.cue"
+	Manifests PackageType = "manifests_tool.cue"
 )
 
 func containsAnyFile(workdir utils_types.FilePath, filename string) bool {
@@ -29,14 +31,19 @@ func containsAnyFile(workdir utils_types.FilePath, filename string) bool {
 	return false
 }
 
-func IdentifyPackage(workdir utils_types.FilePath) Package {
-	if containsAnyFile(workdir, "helm_tool.cue") {
+func IdentifyPackage(workdir utils_types.FilePath) PackageType {
+	if containsAnyFile(workdir, string(Helm)) {
 		return Helm
 	}
 
-	if containsAnyFile(workdir, "manifests_tool.cue") {
+	if containsAnyFile(workdir, string(Manifests)) {
 		return Manifests
 	}
 
+	logus.Log.Panic(
+		"not recognized package type, expected package file",
+		typelog.String("manifests_package_identifier", string(Manifests)),
+		typelog.String("Helm_package_identifier", string(Helm)),
+	)
 	panic("Not recognized kubernetes package")
 }
